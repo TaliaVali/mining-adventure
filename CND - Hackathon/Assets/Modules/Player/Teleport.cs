d4866caf22c60019338128c8b2b41f84880d41ae
TaliaVali <sabrinamoegelin@web.de>
2022-10-22 20:02:58 +0200

    changed restart script

--- CND - Hackathon/Assets/Modules/Player/Teleport.cs
+++ CND - Hackathon/Assets/Modules/Player/Teleport.cs
@@ -8,14 +8,26 @@ public class Teleport : MonoBehaviour
     public GameObject player;
     public Transform startPosition;
 
+    public GameObject lamp1;
+    public GameObject lamp2;
+    public GameObject lamp3;
+    public GameObject lamp4;
+    public GameObject lamp5;
 
-  
+
     void Update()
     {
         if (Input.GetKeyDown("r")) {
 
             //Debug.Log("´pressed r ");
             player.transform.position = startPosition.transform.position;
+
+
+            lamp1.SetActive(false);
+            lamp2.SetActive(false);
+            lamp3.SetActive(false);
+            lamp4.SetActive(false);
+            lamp5.SetActive(false);
 
         }
     }
